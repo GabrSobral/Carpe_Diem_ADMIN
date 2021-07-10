759f0a426aeacc60bcf2c282d6cdf15f76da5496
Sobraloser <Asgoth55@gmail.com>
2021-07-09 23:50:58 -0300

    feat: add activityContenr component

--- components/ActivityContent/index.tsx
+++ components/ActivityContent/index.tsx
@@ -1,21 +1,50 @@
-import Image from 'next/image'
+import { ConfigButton } from '../ConfigButton'
+import { HeaderContent } from '../HeaderContent'
+import { Player } from '../Player'
 
 import styles from './styles.module.scss'
 
 export function ActivityContent(){
   return(
     <div className={styles.container}>
-      <header>
-        <div>
-          <span>Olá Gabriel</span>
+      <HeaderContent/>
+
+      <main>
+        <div className={styles.title_subtitle}>
+          <h1>Ouça e relaxe</h1>
+          <span className={styles.subtitle}>Ouça uma música relaxante, para esvaziar a sua mente.</span>
+          <span className={styles.created_at}>Criado em: 08/07/2021 ás 15: 32</span>
         </div>
 
-        <button type="button">
-          Criar atividade
+        <div className={styles.description}>
+          <p>Ouça músicas relaxantes, para ezvaziar a sua mente e acalmar o seu subconsciente.</p>
+          <p>Esse tipo de exercício é muito útil para se livrar do estresse e dos pensamentos corriqueiros do dia a dia.</p>
+          <p>Você pode tentar meditar enquanto escuta as músicas, isso fará você ter um maior proveito do exercícío.</p>
 
-          <Image src={} alt="Icone de adicionar"/>
-        </button>
-      </header>
+          <p>Ouça músicas relaxantes, para ezvaziar a sua mente e acalmar o seu subconsciente.</p>
+          <p>Esse tipo de exercício é muito útil para se livrar do estresse e dos pensamentos corriqueiros do dia a dia.</p>
+          <p>Você pode tentar meditar enquanto escuta as músicas, isso fará você ter um maior proveito do exercícío.</p>
+
+          <p>Ouça músicas relaxantes, para ezvaziar a sua mente e acalmar o seu subconsciente.</p>
+          <p>Esse tipo de exercício é muito útil para se livrar do estresse e dos pensamentos corriqueiros do dia a dia.</p>
+          <p>Você pode tentar meditar enquanto escuta as músicas, isso fará você ter um maior proveito do exercícío.</p>
+
+          <p>Ouça músicas relaxantes, para ezvaziar a sua mente e acalmar o seu subconsciente.</p>
+          <p>Esse tipo de exercício é muito útil para se livrar do estresse e dos pensamentos corriqueiros do dia a dia.</p>
+          <p>Você pode tentar meditar enquanto escuta as músicas, isso fará você ter um maior proveito do exercícío.</p>
+        </div>
+
+         <div className={styles.files}>
+          <span>Arquivos: 1</span>
+
+          <div className={styles.audio_files}>
+            <Player/>
+            <Player/>
+          </div>
+        </div>      
+      </main>
+
+      <ConfigButton/>
     </div>
   )
 }
