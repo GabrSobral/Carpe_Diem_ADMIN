f43e9a5d458af3c0642d35fda9163e21931b0987
Sobraloser <Asgoth55@gmail.com>
2021-07-10 17:18:04 -0300

    feat: add ConfigButton

--- components/ConfigButton/index.tsx
+++ components/ConfigButton/index.tsx
@@ -3,18 +3,34 @@ import Image from 'next/image'
 import configSVG from '../../images/config.svg'
 import changeSVG from '../../images/change.svg'
 import trashSVG from '../../images/trash.svg'
+import ViewSVG from '../../images/view.svg'
 
 import styles from './styles.module.scss'
 
-export function ConfigButton(){
+interface ConfigButtonProps{
+  view?: boolean;
+}
+
+export function ConfigButton({ view = false }: ConfigButtonProps){
+
   return(
     <div className={styles.container}>
       <div className={styles.relative}>
       <div className={styles.config_menu}>
-        <button type="button">
-          Alterar 
-          <Image src={changeSVG} alt="Botão de alterar atividade"/>
-        </button>
+
+        {
+          !view ? (
+            <button type="button">
+              Alterar 
+              <Image src={changeSVG} alt="Botão de alterar atividade"/>
+            </button>
+          ) : (
+            <button type="button">
+              Visualizar
+              <Image src={ViewSVG} alt="Botão de alterar atividade"/>
+            </button>
+          )
+        }
 
         <button type="button" className={styles.delete_button}>
           Deletar 
