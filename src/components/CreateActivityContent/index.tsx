d058e211b26875821a33042522f4ca2859c96776
Sobraloser <Asgoth55@gmail.com>
2021-07-13 23:44:33 -0300

    feat: add CreateActivityScreen

--- src/components/CreateActivityContent/index.tsx
+++ src/components/CreateActivityContent/index.tsx
@@ -6,64 +6,159 @@ import plusSVG from '../../images/plus.svg'
 
 import { HeaderContent } from '../HeaderContent'
 import { InputCreate } from '../InputCreate'
+import { SelectButton } from '../SelectButton'
+import { DetailArchiveModal } from '../DetailArchiveModal'
+
+import { useModal } from '../../hooks/useModal'
+import { api } from '../../services/api'
 
 import styles from './styles.module.scss'
-import { SelectButton } from '../SelectButton'
-import { useModal } from '../../hooks/useModal'
+import { useCreateActivity } from '../../hooks/useCreateActivity'
+import { useActivity } from '../../hooks/useActivity'
+
+interface FileProps{
+  id: string;
+  name: string;
+  format: string;
+  duration: number;
+  url: string;
+  author: string;
+}
+interface ArchiveSelected{
+  file: FileProps;
+  index: number
+}
+
+interface Activity{
+  id: string;
+  title: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+  body: string;
+  category: string;
+  files: FileProps[]
+}
 
 export function CreateActivityContent(){
-  const [ title, setTitle ] = useState<string>('')
-  const [ subTitle, setSubTitle ] = useState<string>('')
-  const [ description, setDescription ] = useState<string>('')
-
+  const [ isFilled, setIsFilled ] = useState(false)
+  const { handleAddActivity } = useActivity()
+  const [ isDetailArchiveVisible, setIsDetailArchiveVisible ] = useState<boolean>(false)
+  const [ archiveSelected, setArchiveSelected ] = useState<ArchiveSelected>()
+  const { 
+    category,
+    archives,
+    title,
+    description,
+    handleSetDescription,
+    handleSetSubTitle,
+    handleSetTitle,
+    subTitle,
+    handleRemoveArchive
+  } = useCreateActivity()
   const { handleModalCategory, handleModalArchives } = useModal()
 
-  const active = true
-  
-  function handleSetTitle(value: string){ setTitle(value) }
-  function handleSetSubTitle(value: string){ setSubTitle(value) }
-  function handleSetDescription(value: string){ setDescription(value) }
+  useEffect(() => {
+    title && subTitle && description && category ? setIsFilled(true) : setIsFilled(false)
+  },[ title, subTitle, description, category ])
 
-  function handleSumbit(event: FormEvent){
+  function handleDetailArchive(archive: FileProps, index: number){
+    setArchiveSelected({ file: archive, index })
+    setIsDetailArchiveVisible(!isDetailArchiveVisible)
+  }
+  function handleCloseModal(){ setIsDetailArchiveVisible(!isDetailArchiveVisible) }
+
+  async function handleSumbit(event: FormEvent){
     event.preventDefault()
+
+    title.trim()
+    subTitle.trim()
+    description.trim()
+
+    const newActivity = await api.post('/activity/new', {
+      title,
+      description: subTitle,
+      body: description,
+      category: category?.id
+    })
+
+    archives.forEach(async (file) => {
+      await api.post('/archive-activity/new', {
+        activity: newActivity.data.id,
+        archive: file.id
+      })
+    })
+
+    // const Activity = {
+    //   id: newActivity.data.id,
+    //   title: newActivity.data.title,
+    //   created_at: newActivity.data.created_at,
+    //   updated_at: newActivity.data.updated_at,
+    //   description: newActivity.data.description,
+    //   body: newActivity.data.body,
+    //   category: newActivity.data.body,
+    //   files: 
+    // } as Activity
+
+    // handleAddActivity(Activity)
   }
 
   return(
     <div className={styles.container}>
       <HeaderContent title="Criar Atividade"/>
 
-      <main>
-        {/* <span className={styles.created_at}>Criado em: 08/07/2021 ás 15: 32</span> */}
-        <form onSubmit={handleSumbit}>
-          <InputCreate title="Título:" type="text" setValue={handleSetTitle} value={title}/>
-          <InputCreate title="Subtítulo:" type="text" setValue={handleSetSubTitle} value={subTitle}/>
-          <InputCreate title="Descrição:" type="textarea" setValue={handleSetDescription} value={description}/>
+      { isDetailArchiveVisible && (
+        <DetailArchiveModal 
+          handleCloseModal={handleCloseModal}
+          handleRemoveArchive={handleRemoveArchive}
+          file={archiveSelected}
+        />
+      ) }
 
-          <div className={`${styles.select_container} ${ active  && styles.active}`}>
-            <span>Categoria:</span>
-            <SelectButton isActive={active} title="Música" onClick={handleModalCategory}/>
-          </div>
+        <main>
+          {/* <span className={styles.created_at}>Criado em: 08/07/2021 ás 15: 32</span> */}
+          <form onSubmit={handleSumbit}>
+            <InputCreate title="Título:" type="text" setValue={handleSetTitle} value={title}/>
+            <InputCreate title="Subtítulo:" type="text" setValue={handleSetSubTitle} value={subTitle}/>
+            <InputCreate title="Descrição:" type="textarea" setValue={handleSetDescription} value={description}/>
 
-          <div className={`${styles.select_container} ${ active  && styles.active}`}>
-            <span>Arquivos:</span>
-
-            <div className={styles.archives_list}>
-              <SelectButton isActive={active} title="Video_sobre_exercicios.mp4"/>
-              <SelectButton isActive={active} title="Musica_relaxante.mp3"/>
-
-              <button type="button" className={styles.add_file} onClick={handleModalArchives}>
-                <Image src={plusSVG} alt="Icone de adicionar"/>
-              </button>
-
+            <div className={`${styles.select_container} ${ category && styles.active}`}>
+              <span>Categoria:</span>
+              <SelectButton isActive={category ? true : false} title={category?.name || 'Selecione'} onClick={handleModalCategory}/>
             </div>
-          </div>
 
-          <button type="submit" className={styles.submit_button}>
-            <Image src={saveSVG} alt="Icone de salvar"/>
-            Salvar
-          </button>
-        </form>
-      </main>
+            <div className={`${styles.select_container} ${ archives.length !== 0  && styles.active}`}>
+              <span>Arquivos:</span>
+
+              <div className={styles.archives_list}>
+                {
+                  archives.map((item, index) => (
+                    <SelectButton 
+                      key={item.id}
+                      isActive={archives ? true : false} 
+                      title={item.name || ''}
+                      onClick={() => {handleDetailArchive(item, index); }}
+                    />
+                  ))
+                }
+
+                <button type="button" className={styles.add_file} onClick={handleModalArchives}>
+                  <Image src={plusSVG} alt="Icone de adicionar"/>
+                </button>
+
+              </div>
+            </div>
+
+            <button 
+              type="submit" 
+              className={styles.submit_button}
+              disabled={!isFilled}
+            >
+              <Image src={saveSVG} alt="Icone de salvar"/>
+              Salvar
+            </button>
+          </form>
+        </main>
     </div>
   )
 }
