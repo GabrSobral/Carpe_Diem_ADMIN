91b7cb6b377f40bb570ef00483c71bcbc14d6e40
Sobraloser <Asgoth55@gmail.com>
2021-07-15 23:58:07 -0300

    feat: add package

--- src/@types/Activity.ts
+++ src/@types/Activity.ts
@@ -4,8 +4,9 @@ export interface FileProps {
   format: string;
   duration: number;
   url: string;
-  author?: string
+  size?: string
   category: string;
+  created_at: Date;
 }
 
 export interface Activity extends Index{
