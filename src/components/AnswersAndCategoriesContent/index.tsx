6857a6bee5e1edb1fefb0665d78f9d2eaee9bf20
Sobraloser <Asgoth55@gmail.com>
2021-07-20 19:49:47 -0300

    feat: AnswersAndCategories

--- src/components/AnswersAndCategoriesContent/index.tsx
+++ src/components/AnswersAndCategoriesContent/index.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react";
+import Image from 'next/image'
+
 import { Question } from "../../@types/Activity";
 import { api } from "../../services/api";
+import plusSVG from '../../images/plus.svg'
+
+
 import { HeaderContent } from "../HeaderContent";
 import { QuestionItem } from "../QuestionItem";
+
 import styles from "./styles.module.scss" 
+import { CreateQuestionInput } from "../CreateQuestionInput";
 
 export function AnswersAndCategoriesContent(){
   const [ questions, setQuestions ] = useState<Question[]>([])
-
+  const [ createQuestionIsVisible, setCreateQuestionIsVisible ] = useState(false)
 
   useEffect(() => {
     (async function(){
@@ -16,12 +23,33 @@ export function AnswersAndCategoriesContent(){
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
+  function handleUpdateQuestionState(id: string){
+    const newList = questions.filter((item) => item.id !== id);
+
+    setQuestions(newList)
+  }
+
+  function handleAddQuestionToList(question: Question){
+    setQuestions(prevState => [...prevState, question])
+  }
+
   return(
     <div className={styles.container}>
       <HeaderContent title="Categorias e perguntas"/>
+        <button type='button' onClick={() => setCreateQuestionIsVisible(!createQuestionIsVisible)}>
+          Criar Pergunta
+          <Image src={plusSVG} alt="Icone de adicionar"/>
+        </button>
+        { createQuestionIsVisible && 
+        <CreateQuestionInput handleAddQuestionToList={handleAddQuestionToList}/> }
+        
         <main>
-          {questions.map(item => (
-            <QuestionItem key={item.id}/>
+          {questions.map((item) => (
+            <QuestionItem 
+              key={item.id}
+              question={item}
+              handleUpdateQuestionState={() => handleUpdateQuestionState(item.id)}
+            />
           ))}
         </main>
     </div>
