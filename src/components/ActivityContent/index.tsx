a3c24c1d61a31c1fddc2a0e3074e4cea5279270e
Sobraloser <Asgoth55@gmail.com>
2021-10-06 10:04:04 -0300

    fix: bugs at chart

--- src/components/ActivityContent/index.tsx
+++ src/components/ActivityContent/index.tsx
@@ -50,7 +50,7 @@ export function ActivityContent(){
               loader={<div>Loading Chart</div>}
               data={[
                 ['Feedback', 'Gostaram', 'Não gostaram'],
-                [activity.title, activity.feedback.goodCount, activity.feedback.badCount],
+                [activity.title, activity?.feedback.goodCount || 0, activity?.feedback.badCount || 0],
               ]}
               options={{
                 title: 'Quantidade de feedbacks da atividade',
