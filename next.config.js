2c544762d2430159701f9d963d137a723e446b8a
Gabriel Sobral - BDS DataSolution <gsobral@bdsdatasolution.com.br>
2023-11-27 00:06:35 -0300

    fix: changing base url

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports ={
     domains : ['res.cloudinary.com']
   },
   env: {
-    BASE_URL: "https://carpe-diem-api.herokuapp.com/",
+    BASE_URL: "https://carpediem-api.onrender.com/",
   }
 }
